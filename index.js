361875e2d20e58afbc2b44ef6906cd4c473d45f8
intelheropuck <alocparker1220@gmail.com>
2020-12-18 23:08:59 -0500

    adding function to parse text. destructuring returned post data

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 var wordpress = require( "wordpress" );
 require('dotenv').config()
 
-// console.log(process.env)
-
-//Switch over to dotenv!
 var client = wordpress.createClient({
     url: process.env.URL,
     username: process.env.USER,
     password: process.env.PASSWORD
 });
 
-let wpIDMatch = function(name) {
+function wpIDMatch(name) {
     if (name === "198574942") {
         return "Alex Taylor"
     }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function parseContent(data) {
+    // get length of text
+    // pick a random number from 0 to length-1
+    let text = data.replace(regex, "").trim()
+    let randNum = getRandomInt(text.length-1)
+    let randDiff = text.length-1-randNum
+
+    if (randDiff > 120 - 6){
+        randDiff = 120 - 6
+    }
+
+    parsedString = text.substring(randNum,randDiff+randNum)
+    parsedString = "..." + parsedString + "..."
+
+    return parsedString
 }
 
 let regex = /(<\/?(p|br)>)+|(<!-- \/?wp:paragraph -->)+/gmi
@@ -21,10 +39,12 @@ let regex = /(<\/?(p|br)>)+|(<!-- \/?wp:paragraph -->)+/gmi
 client.getPosts(function( error, posts ) {
     if (error) throw error
     else {
-        for (let i = 0; i < posts.length; i++) {
-            console.log(
-                `Post ${i}: \nTitle: ${posts[i].title}\nLink: ${posts[i].link}\nAuthor: ${wpIDMatch(posts[i].author)}\nContent: \n${posts[i].content.replace(regex," ")}`
-            )
-        }
+        // grab a random post, destructure array, console.log data
+        
+        let {title, author, content, link} = posts[getRandomInt(posts.length)]
+
+        // console.log("\ntitle: "+ title + "\nauthor: " + wpIDMatch(author) + "\ncontent: " + content.replace(regex, "") + "\nlink: " + link)
+
+        console.log(parseContent(content))        
     }
 });
