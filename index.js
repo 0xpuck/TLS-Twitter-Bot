9f3fa48c9b28513e7695a88aa30b4be238ca2c3f
intelheropuck <alocparker1220@gmail.com>
2020-12-20 00:35:25 -0500

    adding compromise package, initial code from random-text-analysis project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const wordpress = require( "wordpress" );
 const Twitter = require('twitter-lite');
+const nlp = require('compromise');
+nlp.extend(require('compromise-sentences'))
 require('dotenv').config()
 
 //wordpress connection
@@ -60,8 +62,67 @@ wpClient.getPosts(function( error, posts ) {
     else {
         // grab a random post, destructure array, console.log data
         let {title, author, content, link} = posts[getRandomInt(posts.length)]
-        
+
+        let documentNLP = nlpGeneral(parseContent(content))
+        let docTags = documentNLP.out('text')
+
+        let sentences = nlpSentences(parseContent(content));    
+        let sentencesTags = sentences.out('tags');
+
+        // get number of sentences
+        // pick a random starting index
+
+        let sentenceNum = sentencesTags.length
+        let randIndex = getRandomInt(0,sentenceNum-1)
+    
+        let posNLP = Object.values(sentencesTags[index]);
+
+        console.log(posNLP)
+
+        // adding "#" to the beginning of each tag so they can be used in .match()
+        for (let i = 0; i < posNLP.length; i++){
+            if (posNLP[i].length === 1) {
+                posNLP[i] = "#" + posNLP[i] 
+            }
+            else {
+                for (let j = 0; j < posNLP[i].length; j++) {
+                    posNLP[i][j] = "#" + posNLP[i][j] 
+                }
+            }
+        }
+
+        for (let i = 0; i < posNLP.length; i++) {
+            // if the value at the index is a string, or single value
+            if (typeof posNLP[i] == "string") {
+
+                // find all words that match the term, pick one random one and concat
+                let match = documentNLP.match(posNLP[i]).json()
+                let randomSelection = match[getRandomInt(0,match.length-1)]
+
+                if (randomSelection != undefined) {
+                    saveString+= randomSelection.text + " "
+                }
+            }
+            // if the value is an object, or has multiple values
+            else {
+                let subArrayStr = ""
+                let subArrayLoopRandom = getRandomInt(0, posNLP[i].length-1)
+
+                // pulling a random number of tags to vary specificity
+                for (let j = 0; j < subArrayLoopRandom; j++){
+                    subArrayStr += " " + posNLP[i][j]
+                }
+                //searching for all words that match tags, pulling a random word from that list
+                let match = documentNLP.match(subArrayStr).json()
+                let randomSelection = match[getRandomInt(0,match.length-1)]
+
+                if (randomSelection != undefined) {
+                    saveString+= randomSelection.text + " "
+                }   
+            }
+        }
+     
         const thread = [parseContent(content), `${title} by ${wpIDMatch(author)} ${link}`];
-        tweetThread(thread).catch(console.error);
+        // tweetThread(thread).catch(console.error);
     }
 }); 
